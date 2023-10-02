05e9a6ba1e3a8523b8a6b7a973f094df2fed1831
junha <goodjunha@gmail.com>
2023-10-02 14:54:23 +0900

    🎱:: 프로젝트 세팅

---
+++ 29CMCollectionView/29CMCollectionView/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  29CMCollectionView
+//
+//  Created by 박준하 on 10/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
